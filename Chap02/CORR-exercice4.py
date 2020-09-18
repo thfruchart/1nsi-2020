3bd25da031d1613cb13864e80ecae200d2145531
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-09-18 15:34:33 +0200

    Corrigé exercice 4

---
+++ Chap02/CORR-exercice4.py
@@ -0,0 +1,5 @@
+﻿# correction de l'exercice 4
+x = 0
+for a in range(1, 101): # a prend les valeurs de 1 à 100 inclus (101 exclu)
+    x = x+a    # x est un accumulateur des valeurs de a
+print('1+2+...+100 = ' , x )
