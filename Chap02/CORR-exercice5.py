1d266620eb9ba65eda8bc727bea9657831a9b193
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-09-18 16:56:05 +0200

    Corrigé exercice 5

---
+++ Chap02/CORR-exercice5.py
@@ -0,0 +1,6 @@
+﻿# correction de l'exercice 5
+total = 0
+n = int(input('Entrer n :'))
+for i in range(1, n+1): # a prend les valeurs de 1 à n inclus (n+1 exclu)
+    total = total+i    # x est un accumulateur des valeurs de a
+print(' 1 + 2 +...+',n,' = ' , total )
