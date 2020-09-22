bd3c9992d7735b1f6535e7ecc221a753602c999a
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-09-22 16:51:17 +0200

    Add files via upload

---
+++ Chap03/CORRIGES/Chap3-EXO1.py
@@ -0,0 +1,8 @@
+﻿n = int(input('Entrer n :'))
+
+for a in range(1, n+1 ) :
+    reste =  n % a
+    if reste == 0 :
+        print(a)
+
+
---
+++ Chap03/CORRIGES/Chap3-EXO2.py
@@ -0,0 +1,15 @@
+﻿n = int(input('Entrer n :'))
+compteur = 0
+
+for a in range(1, n+1 ) :
+    reste =  n % a
+    if reste == 0 :
+        #print(a)
+        compteur = compteur + 1
+
+if compteur == 2:
+    print(n , 'est premier')
+else :
+    print(n , ' NON premier')
+
+
