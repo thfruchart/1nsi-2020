6846b1c268d28c43c0a750ef089c27a65ca74e6b
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-09-25 16:31:35 +0200

    Add files via upload

---
+++ Chap03/CORRIGES/Chap3-EXO3.py
@@ -0,0 +1,13 @@
+﻿n=(int(input("Saisir le chiffre de quilles tomber au première essaie")))
+if n==10:
+    print("STRIKE")
+
+else:
+    d=(int(input("Saisir le chiffre de quilles tomber au deuxième essai")))
+
+    if n+d==10:
+        print("spare")
+    elif n+d>10 :
+        print('valeur impossible')
+    else :
+        print('Score : ', n+d)
