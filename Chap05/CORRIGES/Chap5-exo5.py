3fdfc5dd8690df6e621848d8c090c3db91aca763
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-10-16 17:00:45 +0200

    Add files via upload

---
+++ Chap05/CORRIGES/Chap5-exo5.py
@@ -0,0 +1,8 @@
+﻿def occurrences(v,t):
+    cpt = 0
+    for i in range(len(t)):
+        if v == t[i]:
+            cpt += 1  # cpt = cpt+1
+    return cpt
+
+
---
+++ Chap05/CORRIGES/Chap5-exo6.py
@@ -0,0 +1,7 @@
+﻿def echange(t,i,j):
+    aux = t[i]
+    t[i] = t[j]
+    t[j] = aux
+    return t
+
+print(echange([10,20,30,40], 1,2))
